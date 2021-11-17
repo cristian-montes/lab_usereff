81a86a4d20223a79b9395ee404950ef66e007ad4
Cristian Montes <cmontesmujica@gmail.com>
2021-11-16 19:01:02 -0700

    added dynamic rendering to the header/navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -13,8 +13,34 @@ const Navbar = () => {
               <FaBars/>
             </button>
           </div>
-          <div className="links-container show-container"></div>
-          <ul className='social-icons'></ul>
+          <div className="links-container show-container">
+
+            <ul className='links'>
+              {
+                links.map((link) => {
+                  const { id, url, text } = link;
+                  return(
+                    <li key={id}>
+                      <a href={url}>{text}</a>
+                    </li>
+                  )})
+              }
+            </ul>
+
+          </div>
+
+          <ul className='social-icons'>
+            <li>
+              <a href='https://www.twitter.com'> <FaTwitter/> </a>
+            </li>
+            <li>
+              <a href='https://www.twitter.com'> <FaTwitter/> </a>
+            </li>
+            <li>
+              <a href='https://www.twitter.com'> <FaTwitter/> </a>
+            </li>
+          </ul>
+
         </div>
       </nav>
   )
